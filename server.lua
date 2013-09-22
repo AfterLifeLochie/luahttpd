2e7598c5b55ba60518ac9eac147a5598fb6475d1
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2013-09-22 10:17:22 +1000

    Apply global gpl-v2 license
    This project is licensed under gpl-v2 (previous: proprietary)

--- server.lua
+++ server.lua
@@ -1,3 +1,23 @@
+--[[--------------------------------------------------------------------------
+    luahttpd: Lua-powered httpd server project
+    Copyright (C) 2013  AfterLifeLochie <afterlifelochie@afterlifelochie.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+--------------------------------------------------------------------------]]--
+
+
 -- If you're using different system paths, change this.
 LUA_PATH="lua/?.lua;?.lua;?"
 
